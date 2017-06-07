bb850f141c8866ab03a7674d376ff6948ac5c87a
chenxihou <1493136968@qq.com>
2017-06-07 22:11:35 +0800

    transfer to martix

--- hmm.py
+++ hmm.py
@@ -4,70 +4,63 @@
 import numpy as np
 
 class HMM:
-    def __init__(self, pi, A, B, obs_all):
+    def __init__(self, pi, A, B, O):
         self.pi = pi
-        self.trans = A
-        self.omit = B
-        self.obs_all = obs_all
-        self.status_num = A.shape[0]
+        self.A = A
+        self.B = B
+        self.O = O
+        self.T = len(O)
+        self.N = A.shape[0]
+        self.M = B.shape[1]
 
-    def forward(self, obs_seq):
-        T = len(obs_seq)
-        alpha = np.zeros((T, self.status_num))
-        alpha[0,:] = pi * B[:,self.obs_all.index(obs_seq[0])].T
-        # for i in xrange(self.status_num):
-        #     p[0][i] = self.pi[i] * self.omit[i][self.observation.index(observed[0])]
-        for i in xrange(1,T):
-            for j in xrange(self.status_num):
-                total = 0
-                for k in xrange(self.status_num):
-                    total += alpha[i-1][k] * self.trans[k][j] * self.omit[j][self.obs_all.index(obs_seq[i])]
-                alpha[i][j] = total
-        print alpha
-        return sum(alpha[-1]),alpha
+    def forward(self):
+        alpha = np.zeros((self.T, self.N))
+        alpha[0,:] = pi * B[:,self.O[0]].T
+        for t in xrange(1,self.T):
+            for i in range(self.N):
+                alpha[t][i] = np.dot(alpha[t-1,:], self.A[:,i]) * self.B[i][self.O[t]]
+        pro = sum(alpha[-1])
+        return pro, alpha
 
-    def backword(self, observed):
-        observed_num = len(observed)
-        beta = np.zeros((observed_num, self.status_num), dtype= float)
+    def backword(self):
+        beta = np.zeros((self.T, self.N))
         beta[-1:,:] = 1
-        if observed_num > 1:
-            for i in reversed(range(observed_num - 1)):
-                for j in range(self.status_num):
-                    total = 0
-                    for k in range(self.status_num):
-                        total += self.trans[j][k] * self.omit[k][self.obs_all.index(observed[i+1])] * beta[i+1][k]
-                    beta[i][j] = total
-        result = 0
-        print beta
-        for i in range(self.status_num):
-            result += self.pi[i] * self.omit[i][self.obs_all.index(observed[0])] * beta[0][i]
-        return result
+        if self.T > 1:
+            for t in reversed(range(self.T - 1)):
+                for i in range(self.N):
+                    for j in range(self.N):
+                        beta[t][i] = sum(self.A[i,:] * self.B[:,self.O[t+1]].T * beta[t+1,:])
+        pro = sum(beta[0,:] * self.pi.T * self.B[:,self.O[0]].T)
+        return pro, beta
 
-    def cal_gama(self, alpha, beta, obs_seq):
-        T = len(obs_seq)
-        gama = np.zeros((T, self.status_num), dtype=float)
-        for i in range(T):
-            for j in range(self.status_num):
-                gama[i][j] = alpha[i][j] * beta[i][j] / sum([alpha[i][j] * beta[i][j] for i in range(self.status_num)])
-        epsilon = np.zeros((T-1,self.status_num, self.status_num), dtype=float)
-        for i in range(T-1):
-            for j in range(self.status_num):
-                for k in range(self.status_num):
-                    epsilon[i][j][k] = alpha[i][j] * self.trans[j][k] * self.omit[k][self.obs_all.index(obs_seq[i])] * beta[i+1][k]/\
-                        sum([alpha[i][m] * self.trans[m][n] * self.omit[n][self.obs_all.index(obs_seq[i])] * beta[i+1][n] for m in range(self.status_num) for n in range(self.status_num)])
-        return gama
+    def cal_gama_epsilon(self):
+        gama = np.zeros((self.T, self.N), dtype=float)
+        alpha, beta = self.forward()[1], self.backword()[1]
+        for t in range(self.T):
+            for i in range(self.N):
+                gama[t][i] = alpha[t][i] * beta[t][i] / sum([alpha[t][j] * beta[t][j] for j in range(self.N)])
+        epsilon = np.zeros((self.T - 1, self.N, self.N), dtype=float)
+        for t in range(self.T - 1):
+            for i in range(self.N):
+                for j in range(self.N):
+                    epsilon[t][i][j] = alpha[t][i] * self.A[i][j] * self.B[j][self.O[t+1]] * beta[t+1][j]/\
+                        sum([alpha[t][m] * self.A[m][n] * self.B[n][self.O[t+1]] * beta[t+1][n] for m in range(self.N) for n in range(self.N)])
+        return gama, epsilon
 
 
     def baum_welch_train(self):
         pass
 
     def viterbi(self, observed):
-        observed_num = len(observed)
-        p = np.zeros((observed_num, self.status_num))
-        path = np.zeros((observed_num, self.status_num), dtype=int)
-        for i in xrange(self.status_num):
-            p[0][i] = self.pi[i] * self.omit[i][self.observation.index(observed[0])]
-        for i in xrange(1, observed_num):
+        delta = np.zeros(self.T, self.N)
+        psai = np.zeros(self.T, self.N)
+        # path = np.zeros((observed_num, self.status_num), dtype=int)
+        delta[0,:] = self.pi * self.B[:,self.O[0]].T
+        psai[0,:] = 0
+        for t in range(1, self.T):
+            for i in range(self.N):
+                delta[t][i] = max(delta[t-1:] * self.A[:,i].T)
+
             for j in xrange(self.status_num):
                 target = 0
                 for k in xrange(self.status_num):
@@ -89,16 +82,17 @@ A = np.array([[0.5, 0.2, 0.3],
 B = np.array([[0.5, 0.5],
               [0.4, 0.6],
               [0.7, 0.3]])
-obs_all = ['红', '白']
+O = [0,1,0]
+# obs_all = ['红', '白']
 
-hmm = HMM(pi, A, B, obs_all)
-obs_seq = ['白', '白', '红']
-# observed = ['白', '白', '红']
-# observed = ['红']
-pro = hmm.forward(obs_seq)
-pro_b = hmm.backword(obs_seq)
-# path = hmm.viterbi(observed)
+hmm = HMM(pi, A, B, O)
+obs_seq = ['红', '白', '红']
+alpha, r = hmm.forward()
+print alpha, r
+v = hmm.backword()
+print v,1111
 
-print pro
-print pro_b
+gama, epsilon = hmm.cal_gama_epsilon()
+print gama
+print epsilon
 
