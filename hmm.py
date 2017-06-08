ae2433e9cfbeea084f7b770ca066d20b32c18124
chenxihou <chenxihou@chenxihoudeMacBook-Air.local>
2017-06-08 12:20:35 +0800

    complete!!!

--- hmm.py
+++ hmm.py
@@ -15,7 +15,7 @@ class HMM:
 
     def forward(self):
         alpha = np.zeros((self.T, self.N))
-        alpha[0,:] = pi * B[:,self.O[0]].T
+        alpha[0,:] = self.pi * self.B[:,self.O[0]].T
         for t in xrange(1,self.T):
             for i in range(self.N):
                 alpha[t][i] = np.dot(alpha[t-1,:], self.A[:,i]) * self.B[i][self.O[t]]
@@ -48,32 +48,48 @@ class HMM:
         return gama, epsilon
 
 
-    def baum_welch_train(self):
-        pass
+    def baum_welch_train(self, iter_num):
+        for i in range(iter_num):
+            print i, '#' * 100
+            print 'A::' + str(self.A)
+            print 'B::' + str(self.B)
+            print 'pi::' + str(self.pi)
+            gama, epsilon = self.cal_gama_epsilon()
+            new_pi = np.array([gama[0][i] for i in range(self.N)])
+            new_A = np.zeros(np.shape(self.A))
+            for i in range(self.N):
+                for j in range(self.N):
+                    new_A[i][j] = sum([epsilon[t][i][j] for t in range(self.T - 1)])/sum([gama[t][i] for t in range(self.T -1)])
+            new_B = np.zeros(np.shape(self.B))
+            for i in range(self.N):
+                for k in range(self.M):
+                    new_B[i][k] = sum([gama[t][i] for t in range(self.T) if self.O[t] == k])/sum([gama[t][i] for t in range(self.T)])
+            error = sum([np.sum(np.abs(new_A - self.A)), np.sum(np.abs(new_B - self.B)), np.sum(np.abs(new_pi - self.pi))])
+            print error
+            self.A, self.B, self.pi = new_A, new_B, new_pi
+            print 'A::' + str(self.A)
+            print 'B::' + str(self.B)
+            print 'pi::' + str(self.pi)
+            if error < 0.00001:
+                break
+        print error
 
-    def viterbi(self, observed):
-        delta = np.zeros(self.T, self.N)
-        psai = np.zeros(self.T, self.N)
-        # path = np.zeros((observed_num, self.status_num), dtype=int)
+
+    def viterbi(self):
+        delta = np.zeros((self.T, self.N))
+        psai = np.zeros((self.T, self.N), dtype=int)
         delta[0,:] = self.pi * self.B[:,self.O[0]].T
         psai[0,:] = 0
         for t in range(1, self.T):
             for i in range(self.N):
-                delta[t][i] = max(delta[t-1:] * self.A[:,i].T)
+                tmp = delta[t-1,:] * self.A[:,i].T * self.B[i,self.O[t]]
+                delta[t][i], psai[t][i] = max(tmp), np.argmax(tmp)
+        path = []
+        path.append(np.argmax(delta[-1:]))
 
-            for j in xrange(self.status_num):
-                target = 0
-                for k in xrange(self.status_num):
-                    tmp = p[i-1][k] * self.trans[k][j] * self.omit[j][self.observation.index(observed[i])]
-                    if tmp > target:
-                        target = tmp
-                        p[i][j] = tmp
-                        path[i][j] = k
-        walk = []
-        walk.append(np.argmax(p[-1]))
-        for i in reversed(range(1, observed_num)):
-            walk.insert(0,path[i][walk[0]])
-        return walk
+        for t in reversed(range(1, self.T)):
+            path.insert(0,psai[t][path[0]])
+        return path
 
 pi = np.array([0.2, 0.4, 0.4])
 A = np.array([[0.5, 0.2, 0.3],
@@ -86,13 +102,23 @@ O = [0,1,0]
 # obs_all = ['红', '白']
 
 hmm = HMM(pi, A, B, O)
-obs_seq = ['红', '白', '红']
-alpha, r = hmm.forward()
-print alpha, r
-v = hmm.backword()
-print v,1111
-
-gama, epsilon = hmm.cal_gama_epsilon()
-print gama
-print epsilon
-
+# obs_seq = ['红', '白', '红']
+# alpha, r = hmm.forward()
+# print alpha, r
+# v = hmm.backword()
+# print v,1111
+#
+# gama, epsilon = hmm.cal_gama_epsilon()
+# print gama,11111
+# print epsilon
+# print hmm.viterbi()
+# hmm.baum_welch_train()
+p1 = hmm.forward()[0]
+p2 = hmm.backword()[0]
+print p1,p2
+hmm.baum_welch_train(1000)
+p1 = hmm.forward()[0]
+alpha = hmm.forward()[1]
+print alpha
+p2 = hmm.backword()[0]
+print p1,p2
